b04b1d7a1525c768ab5c221bc77f56c0bab28598
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-20 09:39:56 +0100

    fix: Create object to store API data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 "use strict";
 
 import { WeatherAPI } from "./lib/WeatherAPI";
+import { WeatherData } from "./lib/WeatherData";
 
 const api = new WeatherAPI("98eeef4b2615454eb40115006231909");
-api.getWeatherJSON("london").then((value) => {
+
+api.getWeatherData("london").then((value) => {
   console.log(value);
 });
---
+++ src/lib/WeatherData.js
@@ -0,0 +1,37 @@
+"use strict";
+
+export class WeatherData {
+  get name() {
+    return this._name;
+  }
+
+  get region() {
+    return this._region;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  get tempC() {
+    return this._tempC;
+  }
+
+  get tempF() {
+    return this._tempF;
+  }
+
+  get condition() {
+    return this._condition;
+  }
+
+  constructor(weatherJSON) {
+    this._name = weatherJSON.location.name;
+    this._region = weatherJSON.location.region;
+    this._country = weatherJSON.location.country;
+
+    this._tempC = weatherJSON.current.temp_c;
+    this._tempF = weatherJSON.current.temp_f;
+    this._condition = weatherJSON.current.condition.text;
+  }
+}
