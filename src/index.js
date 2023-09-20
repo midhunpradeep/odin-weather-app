0774a8c98b8bc6347de8d4228bc1e16a7c7b88e7
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-20 10:05:54 +0100

    feat: Display weather data on search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 "use strict";
 
 import { WeatherAPI } from "./lib/WeatherAPI";
-import { WeatherData } from "./lib/WeatherData";
 
 const api = new WeatherAPI("98eeef4b2615454eb40115006231909");
 
-api.getWeatherData("london").then((value) => {
-  console.log(value);
+const weatherOutputWrapper = document.getElementById("weather-output-wrapper");
+const fields = {
+  name: weatherOutputWrapper.querySelector(".name"),
+  region: weatherOutputWrapper.querySelector(".region"),
+  country: weatherOutputWrapper.querySelector(".country"),
+  condition: weatherOutputWrapper.querySelector(".condition"),
+  temperature: weatherOutputWrapper.querySelector(".temperature"),
+};
+
+function updateWeatherOutput(weatherData) {
+  fields.name.textContent = weatherData.name;
+  fields.region.textContent = weatherData.region;
+  fields.country.textContent = weatherData.country;
+  fields.condition.textContent = weatherData.condition;
+  fields.temperature.textContent = weatherData.tempC;
+}
+
+const weatherSearchForm = document.getElementById("weather-search-form");
+const weatherInput = weatherSearchForm.querySelector("input[name='location']");
+
+weatherSearchForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const weatherData = await api.getWeatherData(weatherInput.value);
+  if (!weatherData) return;
+
+  weatherInput.value = "";
+  updateWeatherOutput(weatherData);
 });
